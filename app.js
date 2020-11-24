0143426787e766e50d969303e2ae42bf04e5827e
Jacopo <spaccalimoni@gmail.com>
2020-11-24 12:42:25 +0100

    rtsp-relay part - ancora indirizzo privato

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+
+const { proxy } = require('rtsp-relay')(app);
+
+const handler = proxy({
+  url: `rtsp://192.168.1.58:7447/OluifM93AT7FMb7I`,
+  // if your RTSP stream need credentials, include them in the URL as above
+  verbose: false,
+});
+
+// the endpoint our RTSP uses
+app.ws('/api/stream', handler);
+
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
